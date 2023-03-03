2c341c827dfcbbb6c90fd8fedd0157cf0f9c1618
aslanliIbrahim <ibrahimra@code.edu.az>
2023-03-03 23:37:43 +0400

    feat: go back btn added in posts page :sparkles:

--- src/pages/posts/Posts.jsx
+++ src/pages/posts/Posts.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getPostsByUserIdAction } from "../../redux/slices/postSlice";
 import styles from "./posts.module.css";
 
 const Posts = () => {
   const { userId } = useParams();
   const dispatch = useDispatch();
-
+  const navigate = useNavigate();
   const { posts, isLoading, isDirty } = useSelector((s) => s.postReducer);
 
   const notDirtyButEmpty = !isDirty && !posts.length;
   const isEmpty = !posts.length && isDirty;
+
+  const handleGoBackClick = () => {
+    navigate(-1);
+  };
 
   useEffect(() => {
     dispatch(getPostsByUserIdAction(userId));
@@ -19,22 +23,27 @@ const Posts = () => {
   }, []);
 
   return (
-    <div>
-      <h2>Posts of user with id - {userId}</h2>
-      {notDirtyButEmpty || isLoading ? (
-        <>Loading...</>
-      ) : isEmpty ? (
-        <>No posts found</>
-      ) : (
-        posts.map((p, i) => {
-          return (
-            <div className={styles.postContainer} key={i}>
-              <p>{p.title}</p>
-              <p>{p.body}</p>
-            </div>
-          );
-        })
-      )}
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <h2>Posts of user with id - {userId}</h2>
+        {notDirtyButEmpty || isLoading ? (
+          <>Loading...</>
+        ) : isEmpty ? (
+          <>No posts found</>
+        ) : (
+          posts.map((p, i) => {
+            return (
+              <div className={styles.postContainer} key={i}>
+                <p>{p.title}</p>
+                <p>{p.body}</p>
+              </div>
+            );
+          })
+        )}
+      </div>
+      <div className={styles.right}>
+        <button onClick={handleGoBackClick}>Go back</button>
+      </div>
     </div>
   );
 };
